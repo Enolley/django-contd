3d2bfe5423d4aa8bf341ec364cf12874270b0ae9
enolley <enolleyomukamani@gmail.com>
2023-10-25 12:09:40 +0300

    Second App, Routing, Dynamic Title

---
+++ app2/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "app2"
+urlpatterns = [
+    path('', views.Home, name="Home")
+
+]
--- testapp/urls.py
+++ testapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = "testapp"
 urlpatterns = [
     path('', views.Test, name="Test"),
     path('contact', views.contact, name="Contact"),
